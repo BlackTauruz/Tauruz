51d40aaa05ecce2dbf43b424b381815bf05b2122
Leonardo Cavalari <leo.cav007@gmail.com>
2023-03-19 20:52:08 -0300

    First Commit

---
+++ src/Providers/TauruzProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Blacktauruz\Tauruz\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TauruzProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+    }
+}
